6de7cfe358371bfc7b16c93aa09d4b1a06d594a9
Hongarc <anhhong12121997t@gmail.com>
2018-07-03 21:49:30 +0700

    Remove: something don't need (for web)

--- public/js/index.js
+++
@@ -1,8 +0,0 @@
-const socket = io();
-socket.on('new_msg', msg => {
-    $('#messages').append(
-        $('<li>').prepend(
-            $('<pre>').text(JSON.stringify(msg, null, 2))
-        )
-    );
-});
