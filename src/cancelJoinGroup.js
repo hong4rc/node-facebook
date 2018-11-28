8eba75de0aa1e834c0675d9d319d18210acbe4b8
Ｈｏｎｇａｒｃ <anhhong12121997t@gmail.com>
2018-11-28 18:51:02 +0700

    Remove todo

--- src/cancelJoinGroup.js
+++ src/cancelJoinGroup.js
@@ -3,8 +3,6 @@
 const log = require('kiat-log');
 let groupId;
 
-// TODO: add answer
-
 module.exports = {
     url: 'https://www.facebook.com/ajax/groups/confirm_cancel_join_dialog/',
     init: _groupId => {
