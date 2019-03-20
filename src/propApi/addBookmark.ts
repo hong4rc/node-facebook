b91a0e356703ad59261222cfe83cf2ee8be9c88e
Hongarc <anhhong12121997t@gmail.com>
2019-03-20 12:46:44 +0700

    style: lint project

--- src/propApi/addBookmark.ts
+++ src/propApi/addBookmark.ts
@@ -1,7 +1,7 @@
 import Api, { Arg, Form } from '../Api';
 
-export default function(this: Api, id: Arg): Form {
+export default function (this: Api, id: Arg): Form {
   return this.post('https://www.facebook.com/ajax/bookmark/add/', {
     id: `navItem_${id as string}`,
   });
-};
+}
