315f329a932e10c2bf47f223ace0c8046546ac81
Hongarc <anhhong12121997t@gmail.com>
2019-03-22 16:22:21 +0700

    refactor: sort api function and change typo

--- src/propApi/messageRequest.ts
+++ src/propApi/messageRequest.ts
@@ -1,6 +1,6 @@
 import Api, { Form, Id } from '../Api';
 
-export default function (this: Api, isAccept: boolean,  ...threadIds: Id[]): Form {
+export default function (this: Api, isAccept: boolean, ...threadIds: Id[]): Form {
   const form: Form = {};
   const key = isAccept ? 'inbox' : 'other';
   form[key] = threadIds;
