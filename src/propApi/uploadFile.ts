4f296039e46ead2a3254898726c894adfa3f253d
Hongarc <anhhong12121997t@gmail.com>
2019-03-30 14:16:12 +0700

    fix(api): use formData instead of post to upload file

--- src/propApi/uploadFile.ts
+++ src/propApi/uploadFile.ts
@@ -2,7 +2,7 @@ import { ReadStream } from 'fs';
 import Api, { Form } from '../Api';
 
 export default function (this: Api, attachment: ReadStream): Form {
-  return this.post('https://upload.facebook.com/ajax/mercury/upload.php', {
+  return this.formData('https://upload.facebook.com/ajax/mercury/upload.php', {
     upload_1024: attachment,
     voice_clip: 'true',
   }).then((res: Form): Form => {
