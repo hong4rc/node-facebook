5d97c322de5faea2e94a174cdaa80606fc636353
Hongarc <anhhong12121997t@gmail.com>
2019-04-02 09:03:17 +0700

    fix(api): api.logout check response is parseable

--- src/propApi/logout.ts
+++ src/propApi/logout.ts
@@ -5,7 +5,8 @@ export default async function (this: Api): Promise<Form> {
   const res = await this.post('https://www.facebook.com/bluebar/modern_settings_menu/', {
     pmid: 2,
   });
-  const h = findForm(res.data, 'name=\\\\"h\\\\" value=\\\\"', '\\\\"');
+  const str = res.data || JSON.stringify(res);
+  const h = findForm(str, 'name=\\\\"h\\\\" value=\\\\"', '\\\\"');
 
   return this.post('https://www.facebook.com/logout.php', { ref: 'mb', h });
 }
