7699a168f29116c71ceff2e7fb7b2b4c22cc288c
Hongarc <anhhong12121997t@gmail.com>
2019-05-02 17:14:27 +0700

    fix: don't need catch error when upload file

--- src/propApi/upload-file.ts
+++ src/propApi/upload-file.ts
@@ -5,6 +5,5 @@ export default function (this: Api, attachment: ReadStream): Form {
   return this.formData('https://upload.facebook.com/ajax/mercury/upload.php', {
     upload_1024: attachment,
     voice_clip: 'true',
-  }).then((response: Form): Form => response.payload.metadata[0])
-    .catch(() => ({}));
+  }).then((response: Form): Form => response.payload.metadata[0]);
 }
