2de15d5e3a83a4b864d2aa119825ecaceddcc995
hongarc <aqua.hong.97@gmail.com>
2019-05-07 12:45:24 +0700

    style: add `validOption` for mesage group

--- src/propApi/create-group.ts
+++ src/propApi/create-group.ts
@@ -6,13 +6,20 @@ export interface OptionGroup {
   message?: string;
   pin?: boolean;
 }
-export default function (this: Api, option: OptionGroup, ...threadIds: Id[]): Form {
-  return this.post('https://www.facebook.com/ajax/groups/create_post/', {
+
+export const validOption = (option: OptionGroup): Form => {
+  return {
     name: option.name,
-    members: threadIds,
     privacy: option.privacy || 'close',
     message: option.message,
     add_to_favorites: option.pin ? 'on' : undefined,
+  }
+};
+
+export default function (this: Api, option: OptionGroup, ...threadIds: Id[]): Form {
+  return this.post('https://www.facebook.com/ajax/groups/create_post/', {
+    ...validOption(option),
+    members: threadIds,
   }).then((response: Form) => ({
     id: response.jsmods.require[0][3][0].match(/(?:\/groups\/)(\d*)/)[1],
   }));
